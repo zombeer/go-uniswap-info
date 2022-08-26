f4f9e75bb2aa791cb102bcb1318eb44d4e6454ad
Michael Savchenko <zombeer@gmail.com>
2022-08-26 14:53:09 +0200

    shell script updated

--- build_go_bindings.sh
+++ build_go_bindings.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir gen;
-mkdir build;
+mkdir -p gen;
+mkdir -p build;
 for i in ./v2-core/contracts/interfaces/*.sol; do
 	echo Processing $i...;
 	solc --bin --abi $i -o build --overwrite;
 	fn=${i##*/};
 	fn_only=${fn%.sol};
-	mkdir ./gen/${fn_only};
+	mkdir -p ./gen/${fn_only};
 	abigen --bin=./build/${fn_only}.bin --abi=./build/${fn_only}.abi --pkg=${fn_only} --out=./gen/${fn_only}/${fn_only}.go
 done
